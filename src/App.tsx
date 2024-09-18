c76f30e8023dda4c887cc45eb163dded1aaac6f9
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-09-18 22:52:38 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <p className="read-the-docs text-red-500">
+        Hello World!
+      </p>
+      <Button className='bg-red-500'>Hello World</Button>
+    </>
+  )
+}
+
+export default App
