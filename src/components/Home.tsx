6184e46df559a82a9cb421f6266e223e44bdef29
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-09-19 21:48:22 +0300

    feat: added side images

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar";
+import SideImages from "./SideImages";
 
 const Home = () => {
   return (
-    <div className="bg-mainBg w-full rounded-3xl h-[48rem] shadow-2xl mx-32 px-4">
+    <div className="bg-mainBg w-full rounded-3xl h-[48rem] shadow-2xl mx-64 px-4">
       <Navbar />
+      <SideImages />
     </div>
   );
 };
---
+++ src/components/SideImages.tsx
@@ -0,0 +1,31 @@
+import { Icon } from "@iconify/react";
+
+const SideImages = () => {
+  return (
+    <div className='flex flex-col items-end gap-4'>
+      <div className='w-[14rem] h-[13rem] relative group overflow-hidden rounded-xl cursor-pointer'>
+        <img src="/img/car-rental.jpg" alt="car" className='w-full h-full object-cover rounded-xl duration-300 group-hover:scale-125'/>
+        <div className='text-white text-lg font-medium absolute bottom-2 left-2 flex flex-col gap-1'>
+          <Icon icon="f7:car-fill" className="text-xl"/>
+          <p>CAR RENTALS</p>
+        </div>
+      </div>
+      <div className='w-[14rem] h-[13rem] relative group overflow-hidden rounded-xl cursor-pointer'>
+        <img src="/img/hotel.jpg" alt="car" className='w-full h-full object-cover rounded-xl duration-300 group-hover:scale-125'/>
+        <div className='text-white text-lg font-medium absolute bottom-2 left-2 flex flex-col gap-1'>
+          <Icon icon="mingcute:hotel-line" className="text-xl"/>
+          <p>HOTELS</p>
+        </div>
+      </div>
+      <div className='w-[14rem] h-[13rem] relative group overflow-hidden rounded-xl cursor-pointer'>
+        <img src="/img/travel-package.jpg" alt="car" className='w-full h-full object-cover rounded-xl duration-300 group-hover:scale-125'/>
+        <div className='text-white text-lg font-medium absolute bottom-2 left-2 flex flex-col gap-1'>
+          <Icon icon="icon-park-outline:beach-umbrella" className="text-xl"/>
+          <p>TRAVEL PACKAGES</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SideImages
