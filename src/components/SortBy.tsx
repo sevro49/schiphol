f2697190f66cb86cc08da824c76562fcc91093c2
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-09-21 16:00:40 +0300

    feat: added airlines into sort by

--- src/components/SortBy.tsx
+++ src/components/SortBy.tsx
@@ -8,10 +8,53 @@ import {
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
+const airlines = [
+  {
+    id: 1,
+    name: 'American Airlines',
+    price: '$230'
+  },
+  {
+    id: 2,
+    name: 'Delta Airlines',
+    price: '$230'
+  },
+  {
+    id: 3,
+    name: 'United Airlines',
+    price: '$230'
+  },
+  {
+    id: 4,
+    name: 'Southwest Airlines',
+    price: '$230'
+  },
+  {
+    id: 5,
+    name: 'JetBlue',
+    price: '$230'
+  },
+  {
+    id: 6,
+    name: 'Frontier Airlines',
+    price: '$230'
+  },
+  {
+    id: 7,
+    name: 'Spirit Airlines',
+    price: '$230'
+  },
+  {
+    id: 8,
+    name: 'Alaska Airlines',
+    price: '$230'
+  },
+]
+
 const SortBy = () => {
   return (
-    <div className=''>
-      <div className="flex flex-col gap-3">
+    <div className='min-w-56 h-[28rem] overflow-y-scroll overflow-x-hidden hide-scrollbar'>
+      <div className="w-full flex flex-col gap-3">
         <h3 className='font-semibold'>
           Sort By:
         </h3>
@@ -28,38 +71,63 @@ const SortBy = () => {
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
-      <div className="mt-6">
+      <div className="w-full mt-4">
         <h3 className='font-semibold'>
           Arrival Time
         </h3>
-        <RadioGroup defaultValue="5am-11:59am" className="mt-2">
-          <div className="flex items-center space-x-2 mt-2">
+        <RadioGroup defaultValue="5am-11:59am">
+          <div className="flex items-center space-x-2">
             <RadioGroupItem value="5am-11:59am" id="5am-11:59am" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
             <Label htmlFor="5am-11:59am" className="font-normal">5:00 AM - 11:59 AM</Label>
           </div>
-          <div className="flex items-center space-x-2 mt-2">
+          <div className="flex items-center space-x-2">
             <RadioGroupItem value="12pm-5:59pm" id="12pm-5:59pm" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
             <Label htmlFor="12pm-5:59pm" className="font-normal">12:00 PM - 5:59 PM</Label>
           </div>
         </RadioGroup>
       </div>
-      <div className="mt-6">
+      <div className="w-full mt-4">
         <h3 className='font-semibold'>
           Stops
         </h3>
-        <RadioGroup defaultValue="nonstop" className="mt-2">
-          <div className="flex items-center space-x-2 mt-2">
-            <RadioGroupItem value="nonstop" id="nonstop" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
-            <Label htmlFor="nonstop" className="font-normal">Nonstop</Label>
+        <RadioGroup defaultValue="nonstop">
+          <div className="flex items-center justify-between space-x-2">
+            <div className="flex items-center gap-2">
+              <RadioGroupItem value="nonstop" id="nonstop" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
+              <Label htmlFor="nonstop" className="font-normal flex justify-between">Nontop</Label>
+            </div>
+            <span className="font-normal text-sm">$230</span>
           </div>
-          <div className="flex items-center space-x-2 mt-2">
-            <RadioGroupItem value="1-stop" id="1-stop" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
-            <Label htmlFor="1-stop" className="font-normal flex justify-between w-full">1 Stop <span>$230 </span></Label>
+          <div className="flex items-center justify-between space-x-2">
+            <div className="flex items-center gap-2">
+              <RadioGroupItem value="1-stop" id="1-stop" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
+              <Label htmlFor="1-stop" className="font-normal flex justify-between">1 Stop</Label>
+            </div>
+            <span className="font-normal text-sm">$230</span>
           </div>
-          <div className="flex items-center space-x-2 mt-2">
-            <RadioGroupItem value="2+stops" id="2+stops" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
-            <Label htmlFor="2+stops" className="font-normal">2+ Stops</Label>
+          <div className="flex items-center justify-between space-x-2">
+          <div className="flex items-center gap-2">
+              <RadioGroupItem value="2+stops" id="2+stops" className="aria-checked:bg-primary h-3 w-3 text-primary"/>
+              <Label htmlFor="2+stops" className="font-normal flex justify-between">2+ Stops</Label>
+            </div>
+            <span className="font-normal text-sm">$230</span>
           </div>
+        </RadioGroup>
+      </div>
+      <div className="w-full mt-4">
+        <h3 className='font-semibold'>
+          Airlines Included
+        </h3>
+        <RadioGroup defaultValue="nonstop">
+          {airlines.map((airline) => (
+            <div key={airline.id} className="flex items-center justify-between space-x-2">
+              <div className="flex items-center gap-2">
+                <RadioGroupItem value={airline.name} id={airline.name} className="aria-checked:bg-primary h-3 w-3 text-primary"/>
+                <Label htmlFor={airline.name} className="font-normal flex justify-between">{airline.name}</Label>
+              </div>
+              <span className="font-normal text-sm">{airline.price}</span>
+            </div>
+          ))}
         </RadioGroup>
       </div>
     </div>
