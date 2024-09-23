cd8f897c0d3cb4cc58819c72bb01c749e73e6246
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-09-23 15:52:11 +0300

    feat: made MyFlights responsive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,13 @@ import SideImages from "./SideImages";
 
 const Home = () => {
   return (
-    <div className="w-full h-[36rem] sm:h-[52rem] mx-4 lg:mx-32 xl:mx-40 2xl:mx-64 ">
+    <div className="w-full h-[36rem] sm:h-[52rem] mx-4 lg:mx-32 xl:mx-40 2xl:mx-64">
       <div className="bg-mainBg rounded-3xl shadow-2xl w-full pb-2 sm:pb-4 xl:pb-0">
-      <Navbar />
-      <div className="w-full h-[32.5rem] sm:h-[48.5rem] lg:h-[46.5rem] flex flex-col lg:flex-row gap-4 2xl:gap-7 px-2 pb-2 sm:px-5 overflow-y-scroll hide-scrollbar">
-        <Booking />
-        <SideImages />
-      </div>
+        <Navbar />
+        <div className="w-full h-[32.5rem] sm:h-[48.5rem] lg:h-[46.5rem] flex flex-col lg:flex-row gap-4 2xl:gap-7 px-2 pb-2 sm:px-5 overflow-y-scroll hide-scrollbar">
+          <Booking />
+          <SideImages />
+        </div>
       </div>
     </div>
   );
